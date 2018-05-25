a78a560eb4314338f5a248ab8b00717090a2923f
DarkPlusCompany <elcascador@live.com>
2018-05-24 23:07:30 -0400

    Start

---
+++ Assets/Punching.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punching : MonoBehaviour {
+
+	private Animator anim;
+	public bool punch;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	void PunchOff(){
+		punch = false;
+	
+	}
+
+
+	void Update () {
+		anim.SetBool ("punch", punch);
+		if (Input.GetButton("PunchTest")) {
+			punch = true;
+
+			//yield return new Wait
+		}
+
+		}
+	}
+//}
---
+++ Assets/test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+public class test : MonoBehaviour {
+	
+	public GameObject player;
+	public PlayMakerFSM CombatNinja;
+	public bool enable;
+
+	//public static PlayMakerFSM FindFsmOnGameObject (GameObject go, string fsmName);
+
+
+	void Start () {
+		enable = true;
+		//PlayMakerFSM.FindFsmOnGameObject
+		//public static PlayMakerFSM FindFsmOnGameObject (GameObject go, string fsmName);
+		//player.GetComponent<PlayMakerFSM>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q) && enable) {
+			TestDisable ();
+			enable = false;
+		} else if(Input.GetKeyDown(KeyCode.Q) && !enable ) {
+			TestEnable ();
+			enable = true;
+		}
+	}
+
+	void TestDisable (){
+		CombatNinja.GetComponent<PlayMakerFSM>().enabled = false;
+		//player.GetComponent<CombatNinja>().enabled = false;
+	}
+	void TestEnable (){
+		CombatNinja.GetComponent<PlayMakerFSM>().enabled = true;
+		//player.GetComponent<CombatNinja>().enabled = false;
+	}
+}
